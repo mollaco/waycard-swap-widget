db2229907e1e2c6bea52ab53483ecc4439f91791
mollaco <132152769+mollaco@users.noreply.github.com>
2025-11-15 17:26:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { LiFiWidget } from '@lifi/widget'
-import '@lifi/widget/styles.css'
+
 
 const themeConfig = {
   variant: 'wide',
